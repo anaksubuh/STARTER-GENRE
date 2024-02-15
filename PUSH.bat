5656a25972c1c9d9bfe6a5bc67c35c17496731cc
anaksubuh <johanesseptiawan2828@gmail.com>
2024-02-16 00:18:52 +0700

    Update landing page Genre

---
+++ PUSH.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Update landing page Genre"
+git branch -M main
+git remote add origin https://github.com/anaksubuh/STARTER-GENRE.git
+git push -u origin main
---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import webbrowser
+
+url = 'https://xzld06km-8501.asse.devtunnels.ms/'
+
+webbrowser.open_new_tab(url)
