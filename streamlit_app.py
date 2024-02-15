db1f3de43396dc2a3f9e04ce3e5aa7d074c39b9d
anaksubuh <johanesseptiawan2828@gmail.com>
2024-02-16 00:22:18 +0700

    Update landing page Genre

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import webbrowser
 
-url = 'https://xzld06km-8501.asse.devtunnels.ms/'
+st.set_page_config(
+    page_title='GENRE KOTA MAGELANG',
+    page_icon='logo.png',
+    layout='wide',  
+    initial_sidebar_state='expanded',
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': 'https://www.extremelycoolapp.com/bug',
+         'About': '# This is a header. This is an *extremely* cool app!'
+    }
+)
 
+url = 'https://xzld06km-8501.asse.devtunnels.ms/'
 webbrowser.open_new_tab(url)
